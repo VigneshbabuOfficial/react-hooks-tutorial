b562eb8ea644f3b379ddd428f5ecc425262d9c6d
VigneshbabuA <70185865+VigneshOfficial2020@users.noreply.github.com>
2021-09-14 18:36:43 +0530

    useEffect explained

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  // will be calling for every state updates since doesn't have dependency array values
+  useEffect(() => {
+    // Update the document title using the browser API
+    console.log("useEffect - " + count);
+    document.title = `You clicked ${count} times`;
+  });
+
+  // will be called only once componenet is mounted
+  useEffect(() => {
+    console.log("useEffect - [] - " + count);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  //will be calling once state var - count is updated with new value
+  useEffect(() => {
+    console.log("useEffect - [count] - " + count);
+  }, [count]);
 
   return (
     <div>
